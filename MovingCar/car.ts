1531c3332ebce93acb4a74b9e6d4b3c29db322eb
p4pranav <pranavrai07@gmail.com>
2019-11-28 19:46:40 +0530

    moving car code

---
+++ MovingCar/car.ts
@@ -0,0 +1,15 @@
+// function Onclick(){
+//     this.detectmob();
+//     console.log(" i am clicked");
+
+// }
+
+// function detectmob() {
+//     if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+//         console.log("true")
+//         return true;
+//     } else {
+//         console.log("false")
+//       return false;
+//     }
+//  }
